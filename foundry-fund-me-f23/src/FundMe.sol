03e5396fb2ba86ca65dce2576f4d7772ca2aa95c
rooshee <rooshyroy@gmail.com>
2024-07-30 22:17:18 +0200

    Trying to fix import error

--- foundry-fund-me-f23/src/FundMe.sol
+++ foundry-fund-me-f23/src/FundMe.sol
@@ -4,7 +4,7 @@ pragma solidity 0.8.15;
 // 2. Imports
 
 import {AggregatorV3Interface} from "@chainlink/contracts/src/v0.8/shared/interfaces/AggregatorV3Interface.sol";
-// import {PriceConverter} from "./PriceConverter.sol";
+import {PriceConverter} from "./PriceConverter.sol";
 
 // 3. Interfaces, Libraries, Contracts
 error FundMe__NotOwner();
