a961b7155757990a364e6c511d229c68e9bd8eec
rooshee <rooshyroy@gmail.com>
2024-08-10 22:44:05 +0200

    Refactoring test to disable error

--- foundry-fund-me-f23/script/DeployFundMe.s.sol
+++ foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -1,16 +1,16 @@
 // SPDX-License-Identifier: MIT
 
-pragma solidity ^0.8.18;
+pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
 
 // import {PriceConverter} from "../PriceConverter.sol";
 
-contract DeployFundMe {
+contract DeployFundMe is Script {
     function run() external {
         vm.startBroadcast();
-        FundMe fundMe = new FundMe();
+        new FundMe();
         vm.stopBroadcast();
     }
 }
