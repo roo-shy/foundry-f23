a0e2db172758ccb10c3534616066eeea8b9023e8
rooshee <rooshyroy@gmail.com>
2024-08-24 22:59:06 +0200

    adding helper config

--- foundry-fund-me-f23/script/DeployFundMe.s.sol
+++ foundry-fund-me-f23/script/DeployFundMe.s.sol
@@ -4,11 +4,15 @@ pragma solidity ^0.8.20;
 
 import {Script} from "forge-std/Script.sol";
 import {FundMe} from "../src/FundMe.sol";
+import {HelpConfig} from "./HelperConfig.s.sol";
 
 // import {PriceConverter} from "../PriceConverter.sol";
 
 contract DeployFundMe is Script {
     function run() external {
+        HelpConfig helperConfig = new Helperconfig();
+        address ethUsdPriceFeed = helperConfig.activeNetworkConfig().priceFeed;
+
         vm.startBroadcast();
         new FundMe();
         vm.stopBroadcast();
--- foundry-fund-me-f23/script/HelperConfig.s.sol
+++ foundry-fund-me-f23/script/HelperConfig.s.sol
@@ -1,1 +1,35 @@
 // 1. Deploy mocks when we are on the local anvil chain
+
+pragma solidity ^0.8.15;
+
+import {Script} from "forge-std/Script.sol";
+
+contract HelpConfig {
+    // If we are on a local anvil, we deploy mocks
+    // Otherwise, grab the existing address from the live network
+    NetworkConfig public activeNetworkConfig;
+
+    struct NetworkConfig {
+        address priceFeed; // ETH / USD price feed address
+    }
+
+    constructor() {
+        if (block.chainid == 11155111) {
+            activeNetworkConfig = getSepoliEthConfig();
+        } else {
+            activeNetworkConfig = getAnvilEthConfig();
+        }
+    }
+
+    function getSepoliEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+        NetworkConfig memory sepoliaConfig = NetworkConfig({
+            priceFeed: 0x8A753747A1Fa494EC906cE90E9f37563A8AF630e
+        });
+        return sepoliaConfig;
+    }
+
+    function getAnvilEthConfig() public pure returns (NetworkConfig memory) {
+        // price feed address
+    }
+}
