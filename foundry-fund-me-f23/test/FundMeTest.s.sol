9bfeeb0f7173d276010e1c60ca2c6b3361fcb716
rooshee <rooshyroy@gmail.com>
2024-10-03 21:56:09 +0200

    Can't figure out forge testing, refactoring

--- foundry-fund-me-f23/test/FundMeTest.s.sol
+++ foundry-fund-me-f23/test/FundMeTest.s.sol
@@ -1,20 +1,49 @@
-function testPriceFeedVersionIsAccurate() public {
-    uint256 version = fundMe.getVersion();
-    assertEq(version, 4);
+    function testPriceFeedVersionIsAccurate() public {
+        uint256 version = fundMe.getVersion();
+        assertEq(version, 4);
+    }
+
+    function testFundFailsWithoutEnoughEth() public {
+        vm.expectRevert();
+
+        // assert(This tx fails/reverts)
+        fundMe.fund();
+    }
+
+    function testFundUpdatesFundedDataStructure() public {
+        vm.prank(USER); // The next TX will be sent by USER
+        fundMe.fund{value: SEND_VALUE}();
+
+        uint256 amountFunded = fundMe.getAddressToAmountFunded(USER);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testAddsFunderToArrayOffFunders() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+
+        address funder = fundMe.getFunder(0);
+        assertEq(amountFunded, SEND_VALUE);
+    }
+
+    function testWithDrawWithASingleFunder() public funded {
+        // Arrange
+        uint256 startingOwnerBalance = fundMe.getOwner().balance;
+    }
+
+    modifier funded() {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
+        _;
 }
 
-function testFundFailsWithoutEnoughEth() public {
-    vm.expectRevert();
+    function testOnlyOwnerCanWithdraw() public {
+        vm.prank(USER);
+        fundMe.fund{value: SEND_VALUE}();
 
-    // assert(This tx fails/reverts)
-    fundMe.fund();
-}
+        vm.expectRevert();
+        vm.prank(USER);
+        fundMe.withdraw();
 
-function testFundUpdatesFundedDataStructure() public {
-    fundMe.fund{value: 10e18}();
-}
-
-function testWithDrawWithASingleFunder() public funded {
-    // Arrange
-    uint256 startingOwnerBalance = fundMe.getOwner().balance;
-}
+    }
+} 
