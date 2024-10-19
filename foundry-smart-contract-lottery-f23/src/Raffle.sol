b4ef874cd5d50bbdceebbac4671c7871b1b54e5a
rooshee <rooshyroy@gmail.com>
2024-10-19 10:44:32 +0200

    Erros, events

--- foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -1,3 +1,24 @@
+// Layout of Contract: 
+// version
+// imports
+// errors
+// interfaces, libraries, contracts
+// Type declarations
+// State variables
+// Events
+// Modifiers
+// Functions
+
+// Layout of Functions:
+// constructor
+// receive function (if exists)
+// fallback function (if exists)
+// external functions
+// public functions
+// internal functions
+// private functions
+// view & pure functions
+
 // SPDX-License-Identifier: MIT
 
 pragma solidity ^0.8.18;
@@ -10,14 +31,27 @@ pragma solidity ^0.8.18;
  */
 
 contract Raffle {
-    uint256 private s_entranceFee;
+    error Raffle__NotEnoughEthSent();
+
+    uint256 private immutable s_entranceFee;
+    address payable[] private s_players;
+
+    // what data structure should we use to store the participants?
+
 
     constructor(uint256 entranceFee) {
         i_entranceFee = entranceFee;
     }
 
-    function enterRaffle() public payable {
+    function enterRaffle() external payable {
+        // require(msg.value >= i_entranceFee, "Not enough ETH sent!")
+        if(msg.value < i_entranceFee) {
+            revert Raffle__NotEnoughEthSent();
+        }
 
+        s_players.push(payable(msg.sender));
+        // Events making migration easier
+        // Makes indexing easier for the front end
     }
 
     function pickWinner() public {}
