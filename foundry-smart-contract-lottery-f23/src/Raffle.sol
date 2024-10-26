e375d43ba05c25d0cf68034068ad299b07c2c953
rooshee <rooshyroy@gmail.com>
2024-10-26 23:09:27 +0300

    Adding random number

--- foundry-smart-contract-lottery-f23/src/Raffle.sol
+++ foundry-smart-contract-lottery-f23/src/Raffle.sol
@@ -34,16 +34,37 @@ pragma solidity ^0.8.18;
 contract Raffle {
     error Raffle__NotEnoughEthSent();
 
+    /** State Variables */
+    uint256 private constant REQUEST_CONFIRMATIONS = 3;
+
     uint256 private immutable s_entranceFee;
+    uint256 private immutable s_interval;
+    address private immutable i_vrfCoordinator;
+    bytes32 private immutable i_gasLane;
+    uint64 private immutable i_subscriptionId;
+    uint32 private immutable i_callbackGasLimit;
+
     address payable[] private s_players;
+    uint256 private  s_lastTimeStamp;
 
    /** Events */
    event EnteredRaffle(address indexed player);
 
-
-
-    constructor(uint256 entranceFee) {
+    constructor(
+        uint256 entranceFee, 
+        uint256 interval,
+        address vrfCoordinator, 
+        bytes32 gasLane,
+        uint64 i_subscriptionId,
+        uint32 callbackGasLimit,
+        ) {
         i_entranceFee = entranceFee;
+        i_interval = interval;
+        i_vrfCoordinator = vrfCoordinator;
+        i_gasLane = gasLane;
+        i_subscriptionId = subscriptionId;
+        i_callbackGasLimit = callbackGasLimit;
+        s_lastTimeStamp = block.timestamp;
     }
 
     function enterRaffle() external payable {
@@ -58,8 +79,23 @@ contract Raffle {
         emit EnteredRaffle(msg.sender);
     }
 
-    function pickWinner() public {}
+function pickWinner() public {
+    // check to see if enough time has passed
+    if((block.timestamp - s_lastTimeStamp) < i_interval) {
+       revert();
+    }
+      uint256 requestId = i_vrfCoordinator.requestRandomWords(
+            i_gasLane,
+            i_subscriptionId,
+            REQUEST_CONFIRMATIONS,
+            i_callbackGasLimit,
+            numWords
+        );
 
+    // 1. Request the RNG
+    // 2. Get the random number
+
+}
     /** Getter Function */
 
     function getEntranceFee() external view returns(uint256){
