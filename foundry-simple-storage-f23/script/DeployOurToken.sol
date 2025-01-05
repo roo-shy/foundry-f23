031bc2971d42cd9cf71c841e6783a39a31959092
rooshee <rooshyroy@gmail.com>
2025-01-05 11:36:47 +0100

    adding test

--- foundry-simple-storage-f23/script/DeployOurToken.sol
+++ foundry-simple-storage-f23/script/DeployOurToken.sol
@@ -2,14 +2,16 @@
 
 pragma solidity ^0.8.18;
 
-import {Script} from "forge-std/Script.sol";
+import {Script} from "forge-std/Script.sol"; // Ensure this path is correct and the file exists
 import {OurToken} from "../src/OurToken.sol";
 
 contract DeployOurToken is Script {
     uint256 public constant INITIAL_SUPPLY = 1000 ether;
-    function run() external {
+
+    function run() external returns (OurToken) {
         vm.startBroadcast();
-        OurToken ourToken(INITIAL_SUPPLY);
+        OurToken ot = new OurToken(INITIAL_SUPPLY);
         vm.stopBroadcast();
+        return ot;
     }
 }
